2970c40c859b74e9dc4e7003916c94b14a2e3dd0
Ridwan Al Mahmud <162663392+Ridwan-Al-Mahmud@users.noreply.github.com>
2024-06-27 09:31:23 +0000

    nextjs-blog

---
+++ app/about/page.tsx
@@ -0,0 +1,39 @@
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { siteConfig } from "@/config/site";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "About Me",
+  description: "Information about me",
+};
+
+export default async function AboutPage() {
+  return (
+    <div className="container max-w-6xl py-6 lg:py-10">
+      <div className="flex flex-col items-start gap-4 md:flex-row md:justify-between md:gap-8">
+        <div className="flex-1 space-x-4">
+          <h1 className="inline-block font-black text-4xl lg:text-5xl">
+            About Me
+          </h1>
+        </div>
+      </div>
+      <hr className="my-8" />
+      <div className="flex flex-col md:flex-row gap-8 items-center md:items-start">
+        <div className="min-w-48 max-w-48 flex flex-col gap-2">
+          <Avatar className="h-48 w-48">
+            <AvatarImage src="/avatar.png" alt={siteConfig.author} />
+            <AvatarFallback>JC</AvatarFallback>
+          </Avatar>
+          <h2 className="text-2xl font-bold text-center break-words">
+            {siteConfig.author}
+          </h2>
+          <p className="text-muted-foreground text-center break-words">
+            Full Stack Developer
+          </p>
+        </div>
+        <p className="text-muted-foreground text-lg py-4">I'm a passionate Full Stack Engineer with expertise in both frontend and backend development. With a strong foundation in various programming languages and frameworks, I thrive on creating seamless, efficient, and user-friendly web applications. My journey in tech has been driven by a love for problem-solving and a commitment to continuous learning. Whether it's designing intuitive interfaces or building robust server-side logic, I enjoy tackling challenges across the entire stack to deliver high-quality solutions.
+        </p>
+      </div>
+    </div>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Coding Poets",
+  url: "https://example.com",
+  description: "Dive into the world of backend development with expert tips, in-depth tutorials, and best practices on server-side technologies, architecture, performance optimization, and security. Whether you're a seasoned developer or just starting, this blog offers valuable insights to enhance your backend skills.",
+  author: "Ridwan-Al-Mahmud",
+  links: {
+    twitter: "https://twitter.com/Ridwan-Al-Mahmud",
+    github: "https://github.com/Ridwan-Al-Mahmud",
+    personalSite: "https://Ridwan-Al-Mahmud.github.io/portfolio",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
